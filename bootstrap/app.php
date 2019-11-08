1f9bb3a949764610c5c09b35d7b52f3c3ad2c6bf
Josue Martinez <josuermartinezm@gmail.com>
2019-11-07 20:58:06 -0400

    Parcial

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,6 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: *');
 header('Access-Control-Allow-Headers: *');
 
-date_default_timezone_set('America/Caracas');
 /*
 |--------------------------------------------------------------------------
 | Create The Application
